e73df7c08ec39b861464ca59deccb0a2b79e4958
fz3002 <franc3002@gmail.com>
2025-03-11 10:48:51 +0100

    Add Extention class

---
+++ MP1/src/ObjectPlus.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class ObjectPlus implements Serializable {
+    private static Map<Class, List> extent = new HashMap<>();
+    public static final String EXTENT_FILE_NAME = "naszsuperplik.mas";
+
+    public ObjectPlus() {
+        addToExtent();
+    }
+
+    public static void saveExtent() throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(EXTENT_FILE_NAME))) {
+          oos.writeObject(extent);
+        } finally {
+            System.out.println("Saved extent to file");
+        }
+    }
+
+    public static void loadExtent() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(EXTENT_FILE_NAME))) {
+            extent = (Map<Class, List>) ois.readObject();
+        } finally {
+            System.out.println("Loaded extent from file");
+        }
+    }
+
+    public static <T> List<T> getExtentFromClass(Class<T> type) {
+        extent.computeIfAbsent(type, k -> new ArrayList<>());
+        return Collections.unmodifiableList(extent.get(type));
+    }
+
+    protected void addToExtent() {
+        List list = extent.computeIfAbsent(this.getClass(), k -> new ArrayList<>());
+        list.add(this);
+    }
+
+    public void removeFromExtent() {
+        List list = extent.get(this.getClass());
+        if (list != null) {
+            list.remove(this);
+        }
+    }
+}
